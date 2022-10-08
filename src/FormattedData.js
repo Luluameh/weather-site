62de2e69609e6608df8ac0112f2680f2f6526dba
unknown <amehajumi3@gmail.com>
2022-10-08 21:52:41 +0100

    new

--- src/FormattedData.js
+++ src/FormattedData.js
@@ -11,12 +11,14 @@ export default function FormattedDate(props) {
      'Saturday' ]
     let day = days[props.date.getDay()];
 
-    let hours= props.date.gethours();
+    let hours = props.date.getHours();
+
     if (hours <10) {
         hours= `0${hours}`;
         
     }
-    let minutes= props.date.getminutes();
+    let minutes = props.date.getMinutes();
+
     if (minutes <10) {
         minutes= `0${minutes}`;
         
